d96b988e9c51b5e6dc60f6b10db23c2372920031
VladDobychin <vladyslav.dobychin@binary-studio.com>
2026-01-31 17:48:58 +0100

    tutorial-setup: add tutorial before starting the main game

--- play.php
+++ play.php
@@ -5,16 +5,33 @@ require_once 'vendor/autoload.php';
 use Puzzle\Board\BoardFactory;
 use Puzzle\Board\BoardShuffler;
 use Puzzle\Game\Game;
+use Puzzle\Game\Tutorial;
 use Puzzle\InputHandler\InputHandler;
 use Puzzle\Renderer\Renderer;
 use Puzzle\Renderer\VisualComponent;
 
-$boardFactory = new BoardFactory(new BoardShuffler());
+$renderer = new Renderer(
+    new VisualComponent()
+);
+$inputHandler = new InputHandler();
+$boardFactory = new BoardFactory(
+    new BoardShuffler()
+);
+
+$tutorial = new Tutorial(
+    $renderer,
+    $inputHandler,
+    $boardFactory
+);
+
+if ($tutorial->shouldShow()) {
+    $tutorial->run();
+}
 
 $game = new Game(
     $boardFactory->createShuffled(),
-    new Renderer(new VisualComponent()),
-    new InputHandler()
+    $renderer,
+    $inputHandler
 );
 
 $game->start();

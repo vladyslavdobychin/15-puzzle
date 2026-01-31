9ae7e5b14940a050586df8096b3f0c2d15065343
VladDobychin <vladyslav.dobychin@binary-studio.com>
2026-01-31 12:33:19 +0100

    renderer-setup: add docs

--- src/Renderer/VisualComponent.php
+++ src/Renderer/VisualComponent.php
@@ -4,11 +4,22 @@ namespace Puzzle\Renderer;
 
 readonly class VisualComponent
 {
+    /**
+     * Separator for game messages
+     *
+     * @return string
+     */
     public function messageSeparator(): string
     {
         return "+------------------------+\n";
     }
 
+    /**
+     * Border of a tile
+     *
+     * @param int|null $tile
+     * @return string
+     */
     public function tile(?int $tile): string
     {
         $display = $tile === null ? "  " : sprintf('%2d', $tile);
